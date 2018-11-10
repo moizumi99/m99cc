1ca952bf564ff5c92768bc6c8d930771dda0cf6e
Munenori Oizumi <oizumi@be.to>
2018-11-10 15:24:54 -0800

    Fix a bug relating to divider parsing.

--- 9cc.c
+++ 9cc.c
@@ -142,7 +142,7 @@ void tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-' || *p == '*' || *p == '\\' || *p == '(' || *p == ')') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
       tokens[i].ty = *p;
       tokens[i].input = p;
       i++;
