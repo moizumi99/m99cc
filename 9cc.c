9c0f5fd794b3c35451fe478c6313b1bc4154bff0
Munenori Oizumi <oizumi@be.to>
2018-11-10 15:20:58 -0800

    Step 4, supprt parenthesis, multiple. Include abstruct syntax tree.

--- 9cc.c
+++ 9cc.c
@@ -6,18 +6,131 @@
 // Token values
 enum {
   TK_NUM = 256, // integer token
-  TK_EOF,       // end of input token
+  TK_EOF
 };
 
 // Token types
 typedef struct {
-  int ty;   // token type
-  int val;  // value if ty is TK_NUM
-  char *input; // token string for error message
+  int ty;      // token type
+  int val;     // value if ty is TK_NUM
+  char *input;
 } Token;
 
 // Store tokens in this array. Max is 100 for now.
 Token tokens[100];
+
+int pos;
+
+typedef struct Node {
+  int ty;
+  struct Node *lhs;
+  struct Node *rhs;
+  int val;
+} Node;
+
+// Error reporting function.
+void error(char *s, char *message) {
+  fprintf(stderr, s, message);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+Node *new_node(int op, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = op;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *expr();
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = TK_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *term() {
+  if (tokens[pos].ty == TK_NUM) {
+    return new_node_num(tokens[pos++].val);
+  }
+  if (tokens[pos].ty == '(') {
+    pos++;
+    Node *node = expr();
+    if (tokens[pos].ty != ')') {
+      error("No right parenthesis corresponding to left parenthesis (term): %s",
+            tokens[pos].input);
+    }
+    pos++;
+    return node;
+  }
+}
+
+Node *mul() {
+  Node *lhs = term();
+  if (tokens[pos].ty == TK_EOF) {
+    return lhs;
+  }
+  if (tokens[pos].ty == '*') {
+    pos++;
+    return new_node('*', lhs, mul());
+  }
+  if (tokens[pos].ty == '/') {
+    pos++;
+    return new_node('/', lhs, mul());
+  }
+  //error("Unexpected token (mul): %s", tokens[pos].input);
+  return lhs;
+}
+
+Node *expr() {
+  Node *lhs = mul();
+  if (tokens[pos].ty == TK_EOF) {
+    return lhs;
+  }
+  if (tokens[pos].ty == '+') {
+    pos++;
+    return new_node('+', lhs, expr());
+  }
+  if (tokens[pos].ty == '-') {
+    pos++;
+    return new_node('-', lhs, expr());
+  }
+  // error("Unexpected token (expr): %s", tokens[pos].input);
+  return lhs;
+}
+
+void gen(Node *node) {
+  if (node->ty == TK_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty) {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  case '*':
+    printf("  mul rdi\n");
+    break;
+  case '/':
+    printf("  mov rdx, 0\n");
+    printf("  div rdi\n");
+  }
+
+  printf("  push rax\n");
+}
 
 // split chars pointed by p into tokens
 void tokenize(char *p) {
@@ -29,7 +142,7 @@ void tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '\\' || *p == '(' || *p == ')') {
       tokens[i].ty = *p;
       tokens[i].input = p;
       i++;
@@ -53,60 +166,26 @@ void tokenize(char *p) {
   tokens[i].input = p;
 }
 
-// Error reporting function.
-void error(int i) {
-  fprintf(stderr, "Unexpected token: %s\n",
-          tokens[i].input);
-  exit(1);
-}
-             
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "Arguments number not right.\n");
     return 1;
   }
 
-  // Tokenize.
+  // Tokenize and parse.
   tokenize(argv[1]);
-  
+  Node *node = expr();
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  // The head of the equation must be a number.
-  // Check it and output the first mov.
-  if (tokens[0].ty != TK_NUM) {
-    error(0);
-  }
-  printf("  mov rax, %d\n", tokens[0].val);
+  // Generate codes while goind down the abstact tree.
+  gen(node);
 
-  // Find '+ <number>' or '- <number>' tokens,
-  // and output corresponding Assembly codes.
-  int i = 1;
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(i);
-      }
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-    
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(1);
-      }
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    error(i);
-  }
-  
+  // The value on the top of the stack is the final value.
+  // Load it to rax as the final value.
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
--- note.md
+++ note.md
@@ -15,4 +15,44 @@ git remote add origin https://github.com/moizumi99/9cc.git
 git push -u origin master
 ```
 
+# step 2
+
+# step 3
+
+```Token`` should be 
+
+```c
+// Token values
+enum {
+  TK_NUM = 256, // integer token
+  TK_EOF
+};
+```
+
+```Node``` probably needs ```int op```
+
+```c
+typedef struct Node {
+  int ty;           // 演算子かND_NUM
+  struct Node *lhs; // 左辺
+  struct Node *rhs; // 右辺
+  int val;          // tyがND_NUMの場合のみ使う
+  int op;
+} Node;
+```
+
+```error()``` should be like this.
+
+```c
+void error(char *s, int i) {
+  fprintf(stderr, s, tokens[i].input);
+  exit(1);
+}
+```
+
+```mul()``` needs ```tokens[pos].ty```
+
+Inside ```new_node()``` and ```new_node_num()```, ```node->op``` should be ```node->ty```.
+
+### At the end of ```expr()``` and ```term()```, they should return ```lhs```;
 
