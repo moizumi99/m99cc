0d702f7419df2bd47afd9174f00f61286940fed6
Munenori Oizumi <oizumi@be.to>
2018-11-10 12:23:05 -0800

    Adder and subtractor are added.

--- 9cc.c
+++ 9cc.c
@@ -2,15 +2,34 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Arguments number not right.\n");
-        return 1;
+  if (argc != 2) {
+    fprintf(stderr, "Arguments number not right.\n");
+    return 1;
+  }
+  
+  char *p = argv[1];
+  
+  printf(".intel_syntax noprefix\n");
+  printf(".global main\n");
+  printf("main:\n");
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+  
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
     }
 
-    printf(".intel_syntax noprefix\n");
-    printf(".global main\n");
-    printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
-    printf("  ret\n");
-    return 0;
+    fprintf(stderr, "Unexpected letter: '%c'\n", *p);
+    return 1;
+  }
+  
+  printf("  ret\n");
+  return 0;
 }
